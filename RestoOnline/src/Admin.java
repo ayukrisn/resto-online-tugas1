96e1bb2fbc50edf03d62bc0e2842eccbd2f688fd
ayukrisn <ayuukrisn@gmail.com>
2023-04-04 07:55:15 +0800

    Added Login.java and Admin.java file (not finished yet)

---
+++ RestoOnline/src/Admin.java
@@ -0,0 +1,13 @@
+public class Admin {
+    // Variables
+    private String username = null;
+    private String password = null;
+
+    // Constructor
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Methods
+}
